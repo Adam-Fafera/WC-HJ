8ba1ce5462a54284c4a2d888f0bcaba73e16d48d
Fonfi <adam.fafera@edu.uekat.pl>
2024-11-26 16:40:30 +0100

    Added Basic Movement

--- My project/Assets/PlayerMovement.cs
+++ My project/Assets/PlayerMovement.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -13,6 +16,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 0.01f , 0);  
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position = new Vector3(this.transform.position.x - 0.01f, this.transform.position.y , 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 0.01f, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position = new Vector3(this.transform.position.x + 0.01f, this.transform.position.y, 0);
+        }
     }
 }

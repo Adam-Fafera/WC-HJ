a6bb470a78123bde0fbd076711e7844f4dec71ed
kentirBuh <kentirbuh@gmail.com>
2024-12-09 19:46:05 +0100

    Changed how shooting is handled to Player Input
    Changed how shooting is handled to Player Input for homogenization in how inputs are handled.

--- My project/Assets/Scripts/Weapon Scripts/ShootingManager.cs
+++ My project/Assets/Scripts/Weapon Scripts/ShootingManager.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class ShootingManager : MonoBehaviour
 {
     [SerializeField] GameObject bullet;
     [SerializeField] Transform weaponPos;
+
+    /*
     void Update()
     {
+       
+        
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             if (ItemManagement.Instance.currentWeapon.ammo > 0)
@@ -16,6 +21,17 @@ public class ShootingManager : MonoBehaviour
                 ItemManagement.Instance.UpdateAmmo(-1);
                 Instantiate(bullet, weaponPos.transform.position, this.transform.rotation);
             }
+        }
+        
+    }
+    */
+    private void OnFire(InputValue value)
+    {
+        Debug.Log("shoot");
+        if (ItemManagement.Instance.currentWeapon.ammo > 0)
+        {
+            ItemManagement.Instance.UpdateAmmo(-1);
+            Instantiate(bullet, weaponPos.transform.position, this.transform.rotation);
         }
     }
 }

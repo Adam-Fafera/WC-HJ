d4d9c0b1684d272b9cbb6a29001ad8fa59e58363
Fonfi <adam.fafera@edu.uekat.pl>
2024-12-10 02:54:43 +0100

    Reapply "Merge remote-tracking branch 'origin/main' into ShootingAndMovement"
    This reverts commit cb0338637020ce33beb678ffe957d3be2f4aeea7.

--- My project/Assets/Scripts/Weapon Scripts/ShootingManager.cs
+++ My project/Assets/Scripts/Weapon Scripts/ShootingManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class ShootingManager : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class ShootingManager : MonoBehaviour
     [SerializeField] Transform weaponPos;
     void Update()
     {
+       
+        
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             if (ItemManagement.Instance.currentWeapon.ammo > 0)
@@ -16,6 +19,17 @@ public class ShootingManager : MonoBehaviour
                 ItemManagement.Instance.UpdateAmmo(-1);
                 Instantiate(bullet, weaponPos.transform.position, this.transform.rotation);
             }
+        }
+        
+    }
+    */
+    private void OnFire(InputValue value)
+    {
+        Debug.Log("shoot");
+        if (ItemManagement.Instance.currentWeapon.ammo > 0)
+        {
+            ItemManagement.Instance.UpdateAmmo(-1);
+            Instantiate(bullet, weaponPos.transform.position, this.transform.rotation);
         }
     }
 }

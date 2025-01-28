f486ad113926498c9e9e51a992601194dad7966a
K0f1nn <marcin.fijol@edu.uekat.pl>
2025-01-28 16:46:18 +0100

    game done ;)

--- My project/Assets/Scripts/MenuScripts/PauseMenu.cs
+++ My project/Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -44,7 +44,7 @@ public class PauseMenu : MonoBehaviour
     public void Retry()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(4);
     }
     
 }
